f3f24678570cbf8abdcf8a6206d28c0cbe9a7bcb
hsuan881119 <dfghjkl881119@gmail.com>
2023-02-03 13:08:35 +0800

    Delete not use import

--- QA_HW_220111.py
+++ QA_HW_220111.py
@@ -1,12 +1,10 @@
-from os import system
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
 import openpyxl
-from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from openpyxl.styles import Alignment, Font
 
 
 options = Options()
